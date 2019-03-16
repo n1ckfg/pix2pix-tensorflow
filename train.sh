245d06a6e71835af29d05f7a630797c2730c1e81
n1ckfg <nick@fox-gieg.com>
2019-03-16 18:35:26 -0400

    testing

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python pix2pix.py \
   --mode train \
-  --output_dir files/001_output \
+  --output_dir files/train_output_001 \
   --max_epochs 200 \
-  --input_dir files/001_input \
+  --input_dir files/train_input_001 \
   --which_direction AtoB

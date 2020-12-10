a75aa08c97524ccee7df531890023a280a6146dc
n1ckfg <nick@fox-gieg.com>
2020-12-10 09:19:59 -0500

    shell scripts

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
-pip uninstall tensorflow
-pip install tensorflow-gpu
+#pip uninstall tensorflow
+#pip install tensorflow-gpu
+
+mkdir files
+cd files
+mkdir export
+mkdir export_quantized
+mkdir input
+mkdir model
+mkdir output
+mkdir train
+

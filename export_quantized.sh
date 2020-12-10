38d03d5fe3247de0177ff6b09d28b9ec2063713e
n1ckfg <nick@fox-gieg.com>
2020-12-10 01:39:30 -0500

    shell script

--- export_quantized.sh
+++ export_quantized.sh
@@ -1,4 +1,4 @@
 mkdir files/export_quantized
 cd server/tools
-python export-checkpoint.py --checkpoint ../../files/model --output_file ../../export_quantized/output.pict
+python export-checkpoint.py --checkpoint ../../files/model --output_file ../../files/export_quantized/output.pict
 

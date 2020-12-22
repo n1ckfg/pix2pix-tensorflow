96eb06d4743044ed4cd37ea32411272181fbee58
n1ckfg <nick@fox-gieg.com>
2020-12-22 00:00:33 -0500

    batch size

--- train.sh
+++ train.sh
@@ -3,7 +3,8 @@ python pix2pix.py \
   --output_dir files/model \
   --max_epochs 200 \
   --input_dir files/train \
-  --which_direction AtoB
+  --which_direction AtoB \
+  --batch_size 8
 
 ./export_all.sh
 
